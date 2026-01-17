857b08496a489142fbe073d8ef40f7956a50c305
Levin-Alexey <levin.penza@yandex.ru>
2026-01-17 17:19:41 +0300

    fix recidence menu

--- handlers/catalog.py
+++ handlers/catalog.py
@@ -3,9 +3,13 @@
 """
 from aiogram import Router
 from aiogram.types import CallbackQuery
+from aiogram.fsm.context import FSMContext
 from .catalog_handlers.keyboards import get_catalog_keyboard
 
 router = Router()
+
+# URL картинки для главного меню
+START_IMAGE_URL = "https://optim.tildacdn.com/tild3535-3863-4331-b136-396632393536/-/format/webp/IMG_1358.png.webp"
 
 
 @router.callback_query(lambda c: c.data == "catalog")
@@ -23,11 +27,24 @@ async def catalog_handler(callback: CallbackQuery):
 
 
 @router.callback_query(lambda c: c.data == "back_to_menu")
-async def back_to_menu_handler(callback: CallbackQuery):
+async def back_to_menu_handler(callback: CallbackQuery, state: FSMContext):
     """
     Обработчик кнопки "Назад в меню"
+    Возвращает пользователя в главное меню как при /start
     """
     await callback.answer()
-    # TODO: Отправить пользователя обратно в главное меню
-    await callback.message.answer("◀️ Возврат в главное меню...")
+
+    # Сбрасываем состояние FSM
+    await state.clear()
+
+    # Импортируем здесь чтобы избежать циклических импортов
+    from keyboards import get_main_menu_keyboard
+
+    # Отправляем картинку с главным меню
+    await callback.message.answer_photo(
+        photo=START_IMAGE_URL,
+        caption="Я виртуальный помощник. Выберете пункт меню",
+        reply_markup=get_main_menu_keyboard()
+    )
+
 
